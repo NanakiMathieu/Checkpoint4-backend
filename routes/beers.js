3733c218e2fc034246a6dc9b952f8c656ad08e04
NanakiMathieu <mathieu.nanaki@gmail.com>
2022-02-03 16:31:15 +0100

    add route ajouter

--- routes/beers.js
+++ routes/beers.js
@@ -1,5 +1,6 @@
 const beersRouter = require('express').Router();
 const { findMany, findOne } = require('../models/beers');
+const beers = require('../models/beers');
 
 /**Route qui récupère toutes les bieres dans le BDD */
 beersRouter.get('/', (req, res) => {
@@ -14,5 +15,31 @@ beersRouter.get('/:id', (req, res) => {
     .then(result => res.status(200).json(result))
     .catch(err => console.log(err))
 })
+
+/**Route pour poster une nouvelle biere */
+
+beersRouter.post('/ajouter', (req, res) => {
+  console.log(req.body);
+  const {
+    name,
+    description,
+    image_url,
+    ph,
+  } = req.body;
+  beers.postBiere({
+    name,
+    description,
+    image_url,
+    ph,
+  })
+    .then((result) => {
+      console.log(result);
+      res.status(201).send({ statut: 'bière sauvegardée', ...req.body });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('erreur lors de la sauvegarde de la bière')
+    });
+});
 
 module.exports = beersRouter;
